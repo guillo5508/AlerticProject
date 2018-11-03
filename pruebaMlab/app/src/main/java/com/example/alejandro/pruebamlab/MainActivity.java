400b37deaf996e7e60bb05649b0c9db4bf494f24
Alejandro <alejandro-3012@hotmail.com>
2018-11-03 12:07:53 -0500

    pide permisos

--- pruebaMlab/app/src/main/java/com/example/alejandro/pruebamlab/MainActivity.java
+++ pruebaMlab/app/src/main/java/com/example/alejandro/pruebamlab/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.alejandro.pruebamlab;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.StrictMode;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,12 +16,28 @@ import android.widget.EditText;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final int MY_PERMISSIONS_REQUEST_READ_CONTACTS=1;
+    private final int REQUEST_PERMISSION_PHONE_STATE=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_PERMISSION_PHONE_STATE);
+
+        }
 
 
         Button b1=findViewById(R.id.button13);
